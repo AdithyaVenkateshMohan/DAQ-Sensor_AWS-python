8380f12685e53e520412912cbfac75269f8e0973
Adithya Venkatesh Mohan <mohanah@mail.uc.edu>
2019-01-22 18:02:24 -0500

    the intial setup for the data collection is done

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 from sweeppy import Sweep
+print('enga va ya')
 
 with Sweep('/dev/ttyUSB0') as sweep:
     sweep.start_scanning()
+
+    print 'hello'
 
     for scan in sweep.get_scans():
         print('{}\n'.format(scan))
